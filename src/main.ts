667382a9da75025e32df62f257ba100971cac22c
Andranik Adamyan <andranik.adamyan.dev@gmail.com>
2026-05-21 17:43:01 +0400

    fix: parse PORT as number to ensure correct binding

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT || 3002, '0.0.0.0');
+  await app.listen(Number(process.env.PORT) || 3002, '0.0.0.0');
 }
 bootstrap();
